495f664231345152195a59f74815bdb7a9144b12
Ahmed Shahin <83367407+ahmedshahin9@users.noreply.github.com>
2024-10-07 20:53:36 -0400

    add boot control hal

--- device.mk
+++ device.mk
@@ -25,3 +25,18 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-recovery \
+    android.hardware.boot@1.1-impl
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
