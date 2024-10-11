87f672e047ef9d143bb0a1ca31f52f95cf297bcc
Ahmed Shahin <83367407+ahmedshahin9@users.noreply.github.com>
2024-10-10 20:01:18 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,7 @@ TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := false
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_DEVICE_VERSION := linux_man
 TW_Y_OFFSET := 50
 TW_H_OFFSET := -50
